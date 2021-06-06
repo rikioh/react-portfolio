a6afc04b4132eb77b5fdb28b69db7333010163d7
Rikio Campbell <rikiocamp@gmail.com>
2021-06-05 21:03:27 -0700

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
 
   return (
+      // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      //   <a className="navbar-brand" href="/">Rikio Campbell</a>
+      //   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      //   </button>
+      // </nav>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">Rikio Campbell</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        </button>
-      </nav>
+      <Link className="navbar-brand" to="/">
+        Rikio Campbell
+      </Link>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/Portfolio"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/Portfolio"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     );
 
 }
